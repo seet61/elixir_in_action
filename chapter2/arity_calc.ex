232ea118797547f9d72077c40b401436f4b6b68e
Арефьев Дмитрий Александрович <d.arefev@bft.ru>
2025-07-29 14:33:34 +0300

    function with default value

--- chapter2/arity_calc.ex
+++ chapter2/arity_calc.ex
@@ -1,9 +1,10 @@
 defmodule Calculator do
-  def sum(a) do
-    sum(a, 0)
-  end
+  # def sum(a) do
+  #  sum(a, 0)
+  # end
 
-  def sum(a, b) do
+  # replace first function default value for b
+  def sum(a, b \\ 0) do
     a + b
   end
 end
