b61d45c9a796811b1d1b72a9b4289e9a5e14bda8
Арефьев Дмитрий Александрович <d.arefev@bft.ru>
2025-07-29 14:44:48 +0300

    update Calculator

--- chapter2/arity_calc.ex
+++ chapter2/arity_calc.ex
@@ -1,9 +1,16 @@
 defmodule Calculator do
+  @moduledoc """
+  first example with module and function
+  """
+  # attribute
+  @pi 3.14159
+
   # def sum(a) do
   #  sum(a, 0)
   # end
 
   # replace first function default value for b
+  @spec sum(number, number) :: number
   def sum(a, b \\ 0) do
     a + b
   end
