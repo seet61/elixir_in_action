ea6b7d51827e4dcc5e2ed86a05a10f548fb7b62b
dmitriy.arefev <dmitriy.arefev@restream.ru>
2025-11-26 12:34:14 +0300

    small fix

--- lib/chapter4/todo_import.ex
+++ lib/chapter4/todo_import.ex
@@ -2,19 +2,17 @@ defmodule Chapter4.TodoList.Csvimport do
   alias Chapter4.TodoList
 
   def read_todo!(path) do
-    import_data =
-      File.stream!(path)
-      |> Stream.map(&String.replace(&1, "\n", ""))
-      |> Stream.map(&String.split(&1, ","))
-      |> Stream.map(fn [date, job] ->
-        {String.split(date, "/")
-         |> Enum.map(&String.to_integer(&1)), job}
-      end)
-      |> Stream.map(fn {[year, month, day], job} ->
-        {:ok, date} = Date.new(year, month, day)
-        %{date: date, title: job}
-      end)
-
-    TodoList.new(import_data)
+    File.stream!(path)
+    |> Stream.map(&String.replace(&1, "\n", ""))
+    |> Stream.map(&String.split(&1, ","))
+    |> Stream.map(fn [date, job] ->
+      {String.split(date, "/")
+       |> Enum.map(&String.to_integer(&1)), job}
+    end)
+    |> Stream.map(fn {[year, month, day], job} ->
+      {:ok, date} = Date.new(year, month, day)
+      %{date: date, title: job}
+    end)
+    |> TodoList.new()
   end
 end
