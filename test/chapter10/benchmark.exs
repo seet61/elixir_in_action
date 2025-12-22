81dc7eca28fce6512927707e04618c475af4333e
dmitriy.arefev <dmitriy.arefev@restream.ru>
2025-12-22 17:31:05 +0300

    small Benchee example

---
+++ test/chapter10/benchmark.exs
@@ -0,0 +1,28 @@
+# list = Enum.to_list(1..10_000)
+# map_fun = fn i -> [i, i * i] end
+
+# Benchee.run(%{
+#  "flat_map" => fn -> Enum.flat_map(list, map_fun) end,
+#  "map.flatten" => fn -> list |> Enum.map(map_fun) |> List.flatten() end
+# })
+#
+{:ok, pid} = Chapter6.KeyValueGenServer.start()
+
+Benchee.run(%{
+  "key_value_gen.put" => fn ->
+    Enum.each(
+      1..10_000,
+      fn item ->
+        Chapter6.KeyValueGenServer.put(pid, item, item)
+      end
+    )
+  end,
+  "key_value_gen.get" => fn ->
+    Enum.each(
+      1..10_000,
+      fn item ->
+        Chapter6.KeyValueGenServer.get(pid, item)
+      end
+    )
+  end
+})
